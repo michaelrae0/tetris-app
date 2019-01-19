eb395b3ffdb0697e4c820de431e1cf01d71d00fe
mdrae <mdrae0@gmail.com>
2019-01-18 18:18:13 -0600

    Updates

---
+++ precache-manifest.6662bfdba8496b8562a7ab6b6e1d6339.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "255b5f0b06b199b0211a",
+    "url": "/tetris-app/static/css/main.54d2d3b5.chunk.css"
+  },
+  {
+    "revision": "255b5f0b06b199b0211a",
+    "url": "/tetris-app/static/js/main.255b5f0b.chunk.js"
+  },
+  {
+    "revision": "0f208abc99b566416fbf",
+    "url": "/tetris-app/static/js/1.0f208abc.chunk.js"
+  },
+  {
+    "revision": "bc3a97f51a0935b433ef",
+    "url": "/tetris-app/static/js/runtime~main.bc3a97f5.js"
+  },
+  {
+    "revision": "9e4a270b07efa2c1ebc873bd10ead15f",
+    "url": "/tetris-app/static/media/matte2.9e4a270b.jpg"
+  },
+  {
+    "revision": "09de6e7c37cc3e331da48d4ffa13173e",
+    "url": "/tetris-app/static/media/cyan.09de6e7c.JPG"
+  },
+  {
+    "revision": "00a9b1ebc300950bd4fc21abdaf490ed",
+    "url": "/tetris-app/static/media/green.00a9b1eb.JPG"
+  },
+  {
+    "revision": "bd3787cf2d142fc623989284f124cf9b",
+    "url": "/tetris-app/static/media/matte.bd3787cf.jpg"
+  },
+  {
+    "revision": "ebb738dcb9cb411eacc052299dcc90c4",
+    "url": "/tetris-app/static/media/orange.ebb738dc.JPG"
+  },
+  {
+    "revision": "d708c9fd0ad7bdaf3c92e94d0eb8b996",
+    "url": "/tetris-app/static/media/purple.d708c9fd.JPG"
+  },
+  {
+    "revision": "fdc32aa235fe2be54c119137ee6e8e55",
+    "url": "/tetris-app/static/media/red.fdc32aa2.JPG"
+  },
+  {
+    "revision": "9f5092ff29e7051e3b7a101fae77892f",
+    "url": "/tetris-app/static/media/yellow.9f5092ff.JPG"
+  },
+  {
+    "revision": "165dca09aca78f61a571259647b0a11b",
+    "url": "/tetris-app/index.html"
+  }
+];
